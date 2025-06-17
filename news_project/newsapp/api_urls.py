4297bf3d4b3cd994fd674cddeb181b59876ccb8d
Kamogelo-Dc31 <kamokabza156@gmail.com>
2025-06-17 23:07:53 +0200

    Delete news_project directory

--- news_project/newsapp/api_urls.py
+++
@@ -1,7 +0,0 @@
-# newsapp/api_urls.py
-from django.urls import path
-from .api_views import SubscribedArticlesView
-
-urlpatterns = [
-    path('subscribed-articles/', SubscribedArticlesView.as_view(), name='subscribed_articles'),
-]
